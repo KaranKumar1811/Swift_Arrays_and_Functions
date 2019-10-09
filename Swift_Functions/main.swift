891613d568e0fb178664b4e3e74d5f24d00c60e9
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-09 18:19:24 -0400

    Int commit

---
+++ Swift_Functions/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Swift_Functions
+//
+//  Created by MacStudent on 2019-10-09.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
